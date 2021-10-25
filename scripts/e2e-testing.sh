6ac9d900c21f96618f2fe53e8da8c3e158941b8c
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-10-25 22:03:23 +0700

    Re-add integration test for API

---
+++ scripts/e2e-testing.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+main() {
+  # Variables.
+  API_URL=http://localhost:8080/api/v1
+
+  # Tests for API backend.
+  echo "Running e2e inference test for the API backend..."
+  curl -X POST -H "Content-Type: application/json" -d \
+    '{
+      "symptoms": [
+        {
+          "symptomId": "S1",
+          "weight": 0.5
+        },
+        {
+          "symptomId": "S2",
+          "weight": 0.4
+        },
+        {
+          "symptomId": "S3",
+          "weight": 0.2
+        },
+        {
+          "symptomId": "S4",
+          "weight": 0.6
+        },
+        {
+          "symptomId": "S5",
+          "weight": 0.2
+        },
+        {
+          "symptomId": "S6",
+          "weight": 0.4
+        },
+        {
+          "symptomId": "S7",
+          "weight": 0.8
+        },
+        {
+          "symptomId": "S8",
+          "weight": 0.2
+        },
+        {
+          "symptomId": "S9",
+          "weight": 0.2
+        },
+        {
+          "symptomId": "S10",
+          "weight": 0.4
+        },
+        {
+          "symptomId": "S11",
+          "weight": 0.2
+        },
+        {
+          "symptomId": "S12",
+          "weight": 0.2
+        },
+        {
+          "symptomId": "S13",
+          "weight": 0.8
+        }
+      ]
+    }' \
+  http://localhost:8080/api/v1
+
+  # Another line break.
+  echo
+
+  # Done tests.
+  echo "Finished testing!"
+}
+
+# Execute main.
+main
