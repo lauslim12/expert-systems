e0255cfaa360e9111651f2ae8a9cbc1e6f113370
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-10-26 00:27:54 +0700

    Update knowledge base and fix typo

--- pkg/inference/doc.go
+++ pkg/inference/doc.go
@@ -1,5 +1,5 @@
 // Package inference is used to calculate and infer whether a user is infected or not (with TB) based on an input.
 // Algorithms used are Forward Chaining and Certainty Factor.
 // Our knowledge base in this Expert System is located in the 'knowledge.go' file.
-// That folder contains all of the knowledge used to power this Expert System.
+// That file contains all of the knowledge used to power this Expert System.
 package inference
