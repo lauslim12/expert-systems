9aa028a0c3f37c69397a2c1c2e6df5e2e91b86c3
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-10-26 11:50:19 +0700

    Move 'App.tsx' to 'src'

---
+++ web/src/App.tsx
@@ -0,0 +1,38 @@
+import { chakra, Flex } from '@chakra-ui/react';
+import { memo, useEffect, useState } from 'react';
+
+import DisclaimerModal from './components/DisclaimerModal';
+import Footer from './components/Footer';
+import Response from './types/Response';
+
+const App = () => {
+  const [openDisclaimer, setOpenDisclaimer] = useState(true);
+  const [response, setResponse] = useState({} as Response);
+
+  useEffect(() => {
+    fetch('/api/v1')
+      .then((res) => res.json())
+      .then((data: Response) => setResponse(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <>
+      <DisclaimerModal
+        isOpen={openDisclaimer}
+        onClose={() => setOpenDisclaimer(false)}
+      />
+
+      <Flex h="100vh" direction="column" maxW="1200px" mx="auto">
+        <chakra.div as="main" flex={1} mt={5} mb={5}>
+          <p>Hello, Expert Systems!</p>
+          {response && <p>JSON API response: {JSON.stringify(response)}</p>}
+        </chakra.div>
+
+        <Footer setOpenDisclaimer={() => setOpenDisclaimer(true)} />
+      </Flex>
+    </>
+  );
+};
+
+export default memo(App);
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,7 +2,7 @@ import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
