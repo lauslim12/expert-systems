f4be94f88f4f6d41afaa19cff05e1de0284d9ea7
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-10-26 15:30:51 +0700

    Add focus-visible to ChakraUI

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,3 +1,5 @@
+import 'focus-visible/dist/focus-visible';
+
 import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
