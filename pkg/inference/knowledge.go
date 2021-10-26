eccd02126df7714c9a6263ef606936f693e6c383
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-10-26 22:36:38 +0700

    Update knowledge base

--- pkg/inference/knowledge.go
+++ pkg/inference/knowledge.go
@@ -9,11 +9,23 @@ func getDiseases() []Disease {
 			Description: "Tuberculosis (TB) is a bacterial infection spread through inhaling tiny droplets from the coughs or sneezes of an infected person. It mainly affects the lungs, but it can affect any part of the body, including the tummy (abdomen), glands, bones and nervous system. TB is a potentially serious condition, but it can be cured if it is treated with the right antibiotics. TB is spread from person to person through the air. When people with lung TB cough, sneeze or spit, they propel the TB germs into the air. A person needs to inhale only a few of these germs to become infected. About one-quarter of the world's population has a TB infection, which means people have been infected by TB bacteria but are not (yet) ill with the disease and cannot transmit it.",
 			Treatment:   "TB is a treatable and curable disease. Active, drug-susceptible TB disease is treated with a standard 6-month course of 4 antimicrobial drugs that are provided with information and support to the patient by a health worker or trained volunteer. Without such support, treatment adherence is more difficult. Since 2000, an estimated 66 million lives were saved through TB diagnosis and treatment.",
 			Prevention:  "You can perform several precautions by providing good ventilation, natural light, and keeping everything clean. It is recommended to take vaccinations of TB as well. Vaccinations help you to keep your immune system in prime condition, thus allowing you to resist the virus for a longer time (some up to 15 years). Depending on some external factors, some people are more at risk for being exposed into developing the TB disease.",
-			Source: []string{
-				"https://www.nhs.uk/conditions/tuberculosis-tb/",
-				"https://www.who.int/news-room/fact-sheets/detail/tuberculosis",
-				"https://www.tbalert.org/about-tb/what-is-tb/prevention/",
-				"https://www.cdc.gov/tb/topic/basics/tbprevention.htm",
+			Source: []SourceAndLink{
+				{
+					Name: "NHS",
+					Link: "https://www.nhs.uk/conditions/tuberculosis-tb/",
+				},
+				{
+					Name: "WHO",
+					Link: "https://www.who.int/news-room/fact-sheets/detail/tuberculosis",
+				},
+				{
+					Name: "TBAlert",
+					Link: "https://www.tbalert.org/about-tb/what-is-tb/prevention/",
+				},
+				{
+					Name: "CDC Government",
+					Link: "https://www.cdc.gov/tb/topic/basics/tbprevention.htm",
+				},
 			},
 			Symptoms: []string{
 				"S1",
