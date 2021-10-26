a2ca216a03bf733e1da79ee20a028539536313ab
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-10-26 16:19:45 +0700

    Adjust typing for state

--- web/src/types/UserCertaintyWeight.ts
+++ web/src/types/UserCertaintyWeight.ts
@@ -1,4 +1,9 @@
 /**
+ * Enum of weights, can be used at state to prevent rendering the default value.
+ */
+export type StateCertaintyWeight = 0 | 0.25 | 0.75 | 1 | null;
+
+/**
  * Enum of weights used to define the 'certainty' of a symptom according to the user.
  */
 type UserCertaintyWeight = 0 | 0.25 | 0.75 | 1;
