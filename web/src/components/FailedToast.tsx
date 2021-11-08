977ec401ad6e9708423da1e87a164e59a21fab99
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-11-08 13:55:42 +0700

    Add JSDoc comments

--- web/src/components/FailedToast.tsx
+++ web/src/components/FailedToast.tsx
@@ -1,11 +1,18 @@
 import { UseToastOptions } from '@chakra-ui/react';
 
+/**
+ * Displays a failed toast, if, for a reason, a request fails.
+ *
+ * @param toast - ChakraUI's toast object
+ * @param message - Message to be shown in the toast
+ * @returns Custom, standarized ChakraUI's toast
+ */
 const FailedToast = (
   toast: (options?: UseToastOptions | undefined) => string | number | undefined,
   message: string
 ) =>
   toast({
-    title: 'Failed!',
+    title: 'Error!',
     description: message,
     status: 'error',
     duration: 1500,
